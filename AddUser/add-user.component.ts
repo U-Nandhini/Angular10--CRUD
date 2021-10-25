9dac72dbe2ee4578c207637323b004db5a7d8f97
Nandhini-U <88302784+U-Nandhini@users.noreply.github.com>
2021-10-25 10:26:12 +0530

    Add User Data

---
+++ AddUser/add-user.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../common/api.service';
+import { HomeModel } from '../home.model';
+import { Router } from '@angular/router';
+//import { CommonItemService } from '../common-item.service';
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.css']
+})
+export class AddUserComponent implements OnInit {
+
+  userForm = this.formbuilder.group({
+    Name: [''],
+    Password: [''],
+    Email: [''],
+    Mobile: [''],
+  });
+
+  homeModelobj: HomeModel = new HomeModel();
+  data: any;
+
+  constructor( private formbuilder: FormBuilder,
+    private api: ApiService,
+    private router: Router)
+   // private   service : CommonItemService)
+    { }
+
+  ngOnInit(): void {
+    
+
+  }
+
+  addUser() {
+    this.homeModelobj.Name = this.userForm.value.Name;
+    this.homeModelobj.Password = this.userForm.value.Password;
+    this.homeModelobj.Mobile = this.userForm.value.Mobile;
+    this.homeModelobj.Email = this.userForm.value.Email;
+
+    this.api.postUser(this.homeModelobj).subscribe((res:any) => {
+     
+      alert('User added successfully');
+      this.userForm.reset();
+      this.getUserData();
+      this.router.navigate(['home']);
+    });
+  }
+// To get all the data to the table
+  getUserData() {
+    this.api.getUser().subscribe((res) => {
+      this.data = res;
+     
+    });
+  }
+
+}
